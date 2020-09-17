8dadc097bdcdd3b1bb26c33df12ce70a43939a16
deshpandem10 <deshpandem10@gmail.com>
2020-09-17 23:14:18 +0400

    HomePage.js modified. About.js created.

--- code_files/src/Components/HomePage/HomePage.js
+++ code_files/src/Components/HomePage/HomePage.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
-class HomePage extends Component {
+const GridWrapper = styled.div`
+    display: grid;
+    grid-gap: 10px;
+    margin-top: 1em;
+    margin-left: 6em;
+    margin-right: 6em;
+    grid-template-columns: repeat(12, 1fr);
+    grid-auto-rows: minmax(25px, auto);
+`;
 
-    render () {
-        return (
-            <div>
-                <h2>Hello, jStudio!</h2>
-            </div>
-        )
-    }
+export const Home = (props) => {
+    <GridWrapper>
+        <p>Hello, Madhura!</p>
+        <p>Work in progress.</p>
+        <p>This is paragraph 1 on Home page.</p>
+        <p>This is paragraph 2 on Home page.</p>
+    </GridWrapper>
 }
-
-export default HomePage;
