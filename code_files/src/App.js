394acc126f5488bc0283f11de633edecef58a984
deshpandem10 <deshpandem10@gmail.com>
2020-09-17 22:51:56 +0400

    (1) package.json modified to hold the bootstrap dependencies and router dependencies. (2) Link to support Font Awesome icons added in <head> section in index.html in public folder. (3) Navigation Component created using react bootstrap elements.

--- code_files/src/App.js
+++ code_files/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navigation from './Components/Navigation/Navigation';
+
+class App extends Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Router>
+          <Navigation />
+        </Router>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ code_files/src/Components/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+
+    render () {
+        return (
+            <div>
+                <h2>Hello, jStudio!</h2>
+            </div>
+        )
+    }
+}
+
+export default HomePage;
