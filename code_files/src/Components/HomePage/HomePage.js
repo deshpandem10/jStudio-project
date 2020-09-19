63654f4fd966a9c9c233a3d3596dc09d2a9f0596
deshpandem10 <deshpandem10@gmail.com>
2020-09-19 23:08:06 +0400

    Home page is given background image - scaled, positioned, blurred.

--- code_files/src/Components/HomePage/HomePage.js
+++ code_files/src/Components/HomePage/HomePage.js
@@ -2,21 +2,24 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import './HomePage.css';
 
-const GridWrapper = styled.div`
-    display: grid;
-    grid-gap: 10px;
-    margin-top: 1em;
-    margin-left: 6em;
-    margin-right: 6em;
-    grid-template-columns: repeat(12, 1fr);
-    grid-auto-rows: minmax(25px, auto);
-`;
+import Background from '../../Images/home-background.jpg';
+
+let homeStyleBackground = {
+    width: "100%",
+    height: "35em",
+    backgroundImage: `url(${Background})`,
+
+    //Positioning the image properly
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+
+    //Image blurred
+    filter: "blur(3px)",
+}
 
 export const Home = () => (
-    <div className="homeBackground">
+    <div style={ homeStyleBackground }>
         <p>Hello, Madhura!</p>
-        <p>Work in progress.</p>
-        <p>This is paragraph 1 on Home page.</p>
-        <p>This is paragraph 2 on Home page.</p>
-    </div>
-)
+    </div>        
+);
