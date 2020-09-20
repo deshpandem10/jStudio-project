b556afc4c848e74d0391b6467f084c1f113e6cc3
deshpandem10 <deshpandem10@gmail.com>
2020-09-20 16:18:45 +0400

    Home page modifications in progress. (1) Text content written over the background image. (2) Text content styled.

--- code_files/src/Components/HomePage/HomePage.js
+++ code_files/src/Components/HomePage/HomePage.js
@@ -19,7 +19,14 @@ let homeStyleBackground = {
 }
 
 export const Home = () => (
-    <div style={ homeStyleBackground }>
-        <p>Hello, Madhura!</p>
-    </div>        
+    <div>
+        <div style={ homeStyleBackground }></div> 
+
+        <div className="home-content">
+            <h2>Welcome</h2>
+            <h1 style={{color: "rgb(132 214 253)"}}>J Design's Studio</h1>
+            <p className="homePage-paragraph">Interior & Exterior Design Consultancy</p>
+            <p className="homePage-paragraph">Home, Office & Villa Designs</p>
+        </div>
+    </div>       
 );
