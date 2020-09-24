077108a2c077fb748127a2dd70086dbc3123798f
deshpandem10 <deshpandem10@gmail.com>
2020-09-24 23:13:21 +0400

    Contact us page designed. Form created to hold user name, email address and message. Bootstrap buttons to reset and submit form.

--- code_files/src/Components/ContactPage/Contact.js
+++ code_files/src/Components/ContactPage/Contact.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Form, FormControl } from 'react-bootstrap';
+import './Contact.css';
 
-export const Contact = () => (
-    <div>
-        <h2>Contact Us page under construction...</h2>
-    </div>
-);
+export class Contact extends React.Component {
+
+    render() {
+        return (
+            <div className="contactUs-page">
+                <form id="contact-form">
+                    <div className="row form-group">
+                        <label htmlFor="name" className="col-sm-4 contactForm-label">Name </label>
+                        <input type="text" id="name" className="col-sm-8 form-control" />
+                    </div>
+                    <div className="row form-group">
+                        <label htmlFor="emailAddress" className="col-sm-4 contactForm-label">Email Address </label>
+                        <input type="text" id="emailAddress" className="col-sm-8 form-control" />
+                    </div>
+                    <div className="row form-group">
+                        <label htmlFor="message" className="col-sm-4 contactForm-label">Message </label>
+                        <textarea id="message" className="col-sm-8 form-control" rows="3"></textarea>
+                    </div>
+                    <br/>
+                    <div className="row form-group">
+                        <div className="col-sm-4"></div>
+                        <div className="col-sm-4">
+                            <button className="btn btn-secondary" id="reset-form" type="reset">Cancel</button>                 
+                        </div>
+                        <div className="col-sm-4">
+                            <button className="btn btn-primary" id="submit-form" type="submit">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
