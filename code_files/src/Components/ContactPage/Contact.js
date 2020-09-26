bbcefaaa455d87e6a37f71b6553fc9f92382591d
deshpandem10 <deshpandem10@gmail.com>
2020-09-26 17:16:45 +0400

    Contact-Us page css modifications in progress.

--- code_files/src/Components/ContactPage/Contact.js
+++ code_files/src/Components/ContactPage/Contact.js
@@ -9,10 +9,14 @@ export class Contact extends React.Component {
         return (
             <div style={{marginTop: "5%"}}>
                 <div className="row contactUs-page">
-                    <h2>DO YOU HAVE QUESTIONS?</h2>
+                    <div className="col-sm-12">
+                        <h2>DO YOU HAVE QUESTIONS?</h2>
+                    </div>
                 </div>
                 <div className="row contactUs-page">
-                    <h4>Feel free to contact us!</h4>
+                    <div className="col-sm-12">
+                        <h4>Feel free to contact us!</h4>
+                    </div>
                 </div>
 
                 <div className="row" style={{marginTop: "5em"}}>
@@ -20,24 +24,30 @@ export class Contact extends React.Component {
                     <div className="col-sm-6">
                         <form id="contact-form">
                             <div className="row form-group">
-                                <label htmlFor="name" className="col-sm-4 contactForm-label">Name </label>
-                                <input type="text" id="name" className="col-sm-8 form-control" />
+                                <label htmlFor="name" className="col-sm-3 contactForm-label">Name </label>
+                                <div className="col-sm-7">
+                                    <input type="text" id="name" className="form-control" />
+                                </div>                
                             </div>
                             <div className="row form-group">
-                                <label htmlFor="emailAddress" className="col-sm-4 contactForm-label">Email Address </label>
-                                <input type="text" id="emailAddress" className="col-sm-8 form-control" />
+                                <label htmlFor="emailAddress" className="col-sm-3 contactForm-label">Email Address </label>
+                                <div className="col-sm-7">
+                                    <input type="text" id="emailAddress" className="form-control" />
+                                </div>
                             </div>
                             <div className="row form-group">
-                                <label htmlFor="message" className="col-sm-4 contactForm-label">Message </label>
-                                <textarea id="message" className="col-sm-8 form-control" rows="3"></textarea>
+                                <label htmlFor="message" className="col-sm-3 contactForm-label">Message </label>
+                                <div className="col-sm-7">
+                                    <textarea id="message" className="form-control" rows="3"></textarea>
+                                </div> 
                             </div>
                             <br/>
                             <div className="row form-group">
-                                <div className="col-sm-4"></div>
-                                <div className="col-sm-4">
+                                <div className="col-sm-3"></div>
+                                <div className="col-sm-3">
                                     <button className="btn btn-secondary" id="reset-form" type="reset">Cancel</button>                 
                                 </div>
-                                <div className="col-sm-4">
+                                <div className="col-sm-3">
                                     <button className="btn btn-primary" id="submit-form" type="submit">Submit</button>
                                 </div>
                             </div>
